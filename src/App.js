2c8934693f2a6cb0a6aa9eaf747e4d19469e58db
Brandon Cox <brandonscottcox@gmail.com>
2022-03-17 06:48:58 -0600

    working test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  useTransition,
+  useSpring,
+  useChain,
+  config,
+  animated,
+  useSpringRef,
+} from "@react-spring/web";
+import data from "./data";
+import styles from "./styles.module.css";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [open, set] = useState(false);
+
+  const springApi = useSpringRef();
+  const { size, ...rest } = useSpring({
+    ref: springApi,
+    config: config.stiff,
+    from: { size: "20%", background: "hotpink" },
+    to: {
+      size: open ? "100%" : "20%",
+      background: open ? "white" : "hotpink",
+    },
+  });
+
+  const transApi = useSpringRef();
+  const transition = useTransition(open ? data : [], {
+    ref: transApi,
+    trail: 400 / data.length,
+    from: { opacity: 0, scale: 0 },
+    enter: { opacity: 1, scale: 1 },
+    leave: { opacity: 0, scale: 0 },
+  });
+
+  // This will orchestrate the two animations above, comment the last arg and it creates a sequence
+  useChain(open ? [springApi, transApi] : [transApi, springApi], [
+    0,
+    open ? 0.1 : 0.1,
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <animated.div
+        style={{ ...rest, width: size, height: size }}
+        className={styles.container}
+        onClick={() => set((open) => !open)}
+      >
+        {transition((style, item) => (
+          <animated.div
+            className={styles.item}
+            style={{ ...style, background: item.css }}
+          />
+        ))}
+        <div className={open ? "text" : "noText"}>
+          <p className="React">React</p>
+          <p className="CSS">CSS</p>
+          <p className="JavaScript">JavaScript</p>
+          <p className="HTML">HTML</p>
+          <p className="Node">Node.js</p>
+          <p className="Bootstrap">Bootstrap</p>
+        </div>
+      </animated.div>
     </div>
   );
 }
-
-export default App;
